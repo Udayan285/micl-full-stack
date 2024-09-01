cc4e86144abe812b389139a7fb3f2fe78ad12a06
udayan285 <odoynnath@gmail.com>
2024-09-01 20:28:00 +0600

    update business blade

--- resources/views/backend/dashboard/dashboard.blade.php
+++ resources/views/backend/dashboard/dashboard.blade.php
@@ -14,7 +14,11 @@
                  </div>
                 @endif
                 <div class="row g-4">
-                    <div class="col-sm-12 col-xl-6">
+                    <h1 style="color: #009CFF; text-align:center">
+                        Hello! Welcome to MICL dashboard. From this dashboard you can create,read,update,delete your all articles,images and etc.
+                    </h1>
+
+                    {{-- <div class="col-sm-12 col-xl-6">
                         <div class="bg-light text-center rounded p-4">
                             <div class="d-flex align-items-center justify-content-between mb-4">
                                 <h6 class="mb-0">Worldwide Sales</h6>
@@ -22,16 +26,8 @@
                             </div>
                             <canvas id="worldwide-sales"></canvas>
                         </div>
-                    </div>
-                    <div class="col-sm-12 col-xl-6">
-                        <div class="bg-light text-center rounded p-4">
-                            <div class="d-flex align-items-center justify-content-between mb-4">
-                                <h6 class="mb-0">Salse & Revenue</h6>
-                                <a href="">Show All</a>
-                            </div>
-                            <canvas id="salse-revenue"></canvas>
-                        </div>
-                    </div>
+                    </div> --}}
+                    
                 </div>
             </div>
             <!-- Sales Chart End -->
